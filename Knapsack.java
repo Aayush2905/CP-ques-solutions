edb972fd481b0cf52487fd16a762e22cf48edd80
Avanish474 <60430980+Avanish474@users.noreply.github.com>
2020-10-07 10:01:29 +0530

    0-1Knapsack

---
+++ Knapsack.java
@@ -0,0 +1,24 @@
+class Main
+{
+    public static int knapSack(int[] v, int[] w, int n, int W)
+    {
+        if (W < 0) {
+            return Integer.MIN_VALUE;
+        }
+        if (n < 0 || W == 0) {
+            return 0;
+        }
+        int include = v[n] + knapSack(v, w, n - 1, W - w[n]);
+        int exclude = knapSack(v, w, n - 1, W);
+        return Integer.max(include, exclude);
+    }
+    public static void main(String[] args)
+    {
+        int[] v = { 20, 5, 10, 40, 15, 25 };
+        int[] w = {  1, 2,  3,  8,  7, 4 };
+        int W = 10;
+
+        System.out.println("Knapsack value is " +
+                knapSack(v, w, v.length - 1, W));
+    }
+}
